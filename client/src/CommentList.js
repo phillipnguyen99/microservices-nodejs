d7abf9e28cc48d4f6647f321d6e703652c6ee716
1801040111 <1801040111@s.hanu.edu.vn>
2020-12-14 22:46:47 +0700

    UI: client for the blog

---
+++ client/src/CommentList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import axios from "axios";
+
+export default ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
